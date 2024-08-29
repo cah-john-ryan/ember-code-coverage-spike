d16f8b62a32273e808f7d1b43aad25257e056b47
John (Andy) Ryan <john.ryan@cardinalhealth.com>
2024-08-29 08:17:39 -0400

    Installed and setup ember-cli-code-coverage
    Tested and verified that code coverage was being generated both with and without the service continually running

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -4,6 +4,12 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import { forceModulesToBeLoaded, sendCoverage } from 'ember-cli-code-coverage/test-support';
+
+QUnit.done(async function() {
+  forceModulesToBeLoaded();
+  await sendCoverage();
+});
 
 setApplication(Application.create(config.APP));
 
